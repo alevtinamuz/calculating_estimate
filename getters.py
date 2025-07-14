351ae84922f2648af9155fd909ab6fcec25068ac
Pomidorchics <aleksandra180504@gmail.com>
2025-07-14 16:42:23 +1000

    get_by_substr

--- getters.py
+++ getters.py
@@ -13,7 +13,32 @@ def get_price_by_name(db_path, name: str):
 
 
 def get_materials_by_category(supabase, name_of_category: str):
-    cat_id = supabase.table('material_categories').select('id').eq('name', name_of_category).execute().data[0]['id']
+    cat_id = supabase.table('materials_categories').select('id').eq('name', name_of_category).execute().data[0]['id']
     print(cat_id)
-    response = supabase.table('works').select('*').eq('name', 'Покраска карнизных и торцевых свесов в 2 слоя').execute()
+    response = supabase.table('materials').select('*').eq('category_id', cat_id).execute()
     print(response.data)
+
+    return response.data
+
+
+def get_works_by_category(supabase, name_of_category: str):
+    cat_id = supabase.table('works_categories').select('id').eq('name', name_of_category).execute().data[0]['id']
+    print(cat_id)
+    response = supabase.table('works').select('*').eq('category_id', cat_id).execute()
+    print(response.data)
+
+    return response.data
+
+
+def get_materials_by_substr(supabase, substr):
+    response = supabase.table('materials').select('*').ilike('name', f'%{substr}%').execute()
+    print(response.data)
+
+    return response.data
+
+
+def get_works_by_substr(supabase, substr):
+    response = supabase.table('works').select('*').ilike('name', f'%{substr}%').execute()
+    print(response.data)
+
+    return response.data
