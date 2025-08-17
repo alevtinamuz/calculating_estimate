1df649601d0d4b35bd3a6e336fc1e4b2e6037310
Pomidorchics <aleksandra180504@gmail.com>
2025-08-17 15:14:57 +1000

    fix

--- design/class_ComboBoxDelegate.py
+++ design/class_ComboBoxDelegate.py
@@ -577,7 +577,8 @@ class ComboBoxDelegate(QStyledItemDelegate):
                     model.setData(unit_index, entity['unit'])
                     
                     quantity_index = model.index(index.row(), 3)
-                    model.setData(quantity_index, 1.0)
+                    current_quantity = self.table.item(index.row(), 3).text() if self.table.item(index.row(), 3) else 1.0
+                    model.setData(quantity_index, float(current_quantity))
 
                 elif index.column() == 6:  # Обработка материала
 
@@ -597,7 +598,8 @@ class ComboBoxDelegate(QStyledItemDelegate):
                         model.setData(unit_index, entity['unit'])
                         
                         quantity_index = model.index(index.row(), 8)
-                        model.setData(quantity_index, 1.0)
+                        current_quantity = self.table.item(index.row(), 8).text() if self.table.item(index.row(), 8) else 1.0
+                        model.setData(quantity_index, float(current_quantity))
 
             except IndexError as ie:
                 print(f"Ошибка индекса при обновлении данных: {ie}")
